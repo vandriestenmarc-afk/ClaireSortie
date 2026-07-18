66abb55d73d24587f1fdc5999164a51da7ec73e3
vandriestenmarc-afk <vandriesten.marc@gmail.com>
2026-07-18 21:10:45 +0200

    Clear stale public demo cache and unregister service worker

--- sw.js
+++ sw.js
@@ -1,28 +1,13 @@
-const CACHE = "clairsortie-v3";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./manifest.webmanifest",
-  "./assets/icon.svg",
-  "./src/app.js",
-  "./src/extractor.js",
-  "./src/sample.js"
-];
-
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET" || new URL(event.request.url).pathname.includes("/api/")) return;
-  event.respondWith(fetch(event.request).then(response => {
-    const copy = response.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy));
-    return response;
-  }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html"))));
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll({ type: "window" }))
+      .then(clients => clients.forEach(client => client.navigate(client.url)))
+  );
 });
